99550d271e7d85bb82fb35aea1fa4ee363653282
yuhkiikd <yuhki.ikd@gmail.com>
2019-08-14 10:27:57 +0000

    20190814_002_ツイート用フォーム実装

--- app/controllers/chatters_controller.rb
+++ app/controllers/chatters_controller.rb
@@ -1,4 +1,18 @@
 class ChattersController < ApplicationController
   def index
+    @chatters = Chatter.all
+    @chatter = Chatter.new
   end
+
+  def create
+    Chatter.create(chatter_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def chatter_params
+    params.require(:chatter).permit(:content)
+  end
+
 end
